a18573fd05d95f2de8ab463e52eff565d2a5fae9
Harry <sahilbhaisharma1212@gmail.com>
2025-05-03 02:09:47 +0530

    ui changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,38 @@ import { motion } from "framer-motion";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import unnamedImage from './assets/unnamed.png';
-import { Brush, Circle, Minus, MoveUpRight, Square, Text } from "lucide-react";
+import { Brush, ChevronRight, Circle, FolderKanban, Minus, MoveUpRight, Square, Text } from "lucide-react";
+import pad2 from "./assets/pad2.png"
 
 export default function App() {
   return (
-    <>
+    <div className="relative">
+
+      {/* bg drawing pad */}
+      <motion.div
+        className="bg-cover bg-center bg-no-repeat opacity-15 absolute -right-75 -top-50 rotate-45 z-0"
+        style={{ backgroundImage: `url(${pad2})`, height: 900, width: 900 }}
+        animate={{ x: [-10, 10, -20, 0, 10, -10], y: [-10, 10, -20, 0, 10, -10], rotate: [-2, 2, -3, 0, 2, -2] }}
+        transition={{ repeat: Infinity, duration: 33 }}
+      />
+
+
       {/* navbar */}
-      <nav className="flex justify-between p-2">
+      <nav className="flex justify-between p-2 z-10">
+
         {/* logo */}
         <motion.div
-          className="rounded-full bg-gradient-to-br from-gray-200/80 via-gray-500/80 to-blue-300/15 opacity-75 h-14 w-14 shadow-2xl shadow-gray-500/10"
+          className="rounded-full bg-gradient-to-br from-gray-200/80 via-gray-500/80 to-blue-300/15 opacity-75 h-14 w-14 shadow-2xl shadow-gray-500/10 backdrop-blur-3xl"
           animate={{ rotate: 360 }}
           transition={{ repeat: Infinity, duration: 7, ease: "linear" }}
         />
 
         {/* navbar options */}
         <div className="flex px-2 py-1 bg-gradient-to-b from-gray-400/30 via-gray-500/30 to-blue-300/10 rounded-xl items-center border border-gray-500/50 shadow-2xl shadow-gray-500/10">
-          <div className="flex text-white gap-3 hover:bg-gray-300/10 p-3 px-8 rounded-lg transition duration-300 cursor-pointer">
+        {/* mail */}
+          <a className="flex text-white gap-3 hover:bg-gray-300/10 p-3 px-8 rounded-lg transition duration-300 cursor-pointer" href="https://mail.google.com/mail/?view=cm&to=sahilbhaisharma1212@gmail.com" target="blank">
             <IoMdMail className="text-2xl" /> Email
-          </div>
+          </a>
           {/* github */}
           <a className="flex text-white gap-3 hover:bg-gray-300/10 p-3 px-8 rounded-lg transition duration-300 cursor-pointer" href="https://github.com/sahilSharma1212" target="blank">
             <FaGithub className="text-2xl" />Github
@@ -39,8 +52,11 @@ export default function App() {
         />
       </nav>
 
+
+
+
       {/* hero section */}
-      <div className="flex flex-col items-center w-screen pt-16">
+      <div className="flex flex-col items-center w-screen pt-16 z-10">
 
 
 
@@ -59,14 +75,24 @@ export default function App() {
               {/* square feature */}
               <div className="flex flex-col items-center absolute top-0 left-0">
 
-                <div className="h-12 flex justify-center items-center px-2 py-1 gap-2 ">
+                <motion.div
+                className="h-12 flex justify-center items-center px-2 py-1 gap-2 "
+                animate={{y:[-3,3,-2,4]}}
+                transition={{duration:10,repeat:Infinity}}
+                >
                   <div className="h-10 w-10 flex rounded-full bg-white/20 justify-center items-center">
 
                     <Square strokeWidth={1} className="w-6 h-6" />
                   </div>
                   <p className="">Sqaures</p>
+                </motion.div>
+                <div className="w-0.5 h-40 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full">
+                  <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 144, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
+                  />
                 </div>
-                <div className="w-0.5 h-40 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full"></div>
               </div>
 
 
@@ -81,7 +107,13 @@ export default function App() {
                   <p className="">Arrows</p>
                 </div>
                 {/* line */}
-                <div className="w-0.5 h-24 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full"></div>
+                <div className="w-0.5 h-24 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full">
+                <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 144, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 9, repeat: Infinity, ease: "easeInOut" }}
+                  />
+                </div>
               </div>
 
 
@@ -95,7 +127,13 @@ export default function App() {
                   </div>
                   <p className="">Lines</p>
                 </div>
-                <div className="w-0.5 h-40 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full"></div>
+                <div className="w-0.5 h-40 bg-gradient-to-r from-white/0 via-white/35 to-white/0 rounded-full">
+                <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 144, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
+                  />
+                </div>
               </div>
 
             </div>
@@ -104,8 +142,8 @@ export default function App() {
             <motion.div
               className="w-96 h-96 bg-cover bg-center rounded-lg shadow-2xl"
               style={{ backgroundImage: `url(${unnamedImage})` }}
-              animate={{ y: [4, -2, 3, 1, 0, 5] }}
-              transition={{ duration: 8, repeat: Infinity }}
+              animate={{ y: [4, -2, 3, 1, 0, 5], rotate: [-2, 2, -3, 0, 2, -2] }}
+              transition={{ duration: 15, repeat: Infinity }}
             />
 
             {/* new features section */}
@@ -114,7 +152,14 @@ export default function App() {
               {/* free draw feature */}
               <div className="flex flex-col items-center absolute top-70 left-5">
 
-                <div className="w-0.5 h-32 bg-gradient-to-r from-white/0 via-white/35 to-white/0"></div>
+                <div className="w-0.5 h-32 bg-gradient-to-r from-white/0 via-white/35 to-white/0">
+                
+                <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 100, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 10, repeat: Infinity, ease: "easeInOut" }}
+                  />
+                  </div>
 
 
                 <div className="flex gap-2 items-center">
@@ -132,9 +177,13 @@ export default function App() {
               {/* text feature */}
               <div className="flex flex-col items-center absolute top-70 left-39">
 
-                <div className="w-0.5 h-20 bg-gradient-to-r from-white/0 via-white/35 to-white/0"></div>
-
-
+                <div className="w-0.5 h-20 bg-gradient-to-r from-white/0 via-white/35 to-white/0">
+                <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 50, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 7, repeat: Infinity, ease: "easeInOut" }}
+                  />
+                </div>
                 <div className="flex gap-2 items-center">
 
                   <div className="w-10 h-10 bg-white/10 rounded-full flex items-center justify-center shadow py-1 px-2">
@@ -150,7 +199,13 @@ export default function App() {
               {/* circles feature */}
               <div className="flex flex-col items-center absolute top-70 left-68">
 
-                <div className="w-0.5 h-28 bg-gradient-to-r from-white/0 via-white/35 to-white/0"></div>
+                <div className="w-0.5 h-28 bg-gradient-to-r from-white/0 via-white/35 to-white/0">
+                <motion.div
+                    className="absolute left-1/2 -translate-x-1/2 w-0.5 h-8 bg-white/35 rounded-full"
+                    animate={{ y: [0, 80, 0] }} // Moves from top to bottom and back
+                    transition={{ duration: 8, repeat: Infinity, ease: "easeInOut" }}
+                  />
+                </div>
 
 
                 <div className="flex gap-2 items-center">
@@ -174,7 +229,7 @@ export default function App() {
 
 
             {/* hero heading */}
-            <h1 className="text-4xl font-bold bg-gradient-to-br to-gray-400 via-gray-500 from-gray-300 bg-clip-text text-transparent">
+            <h1 className="text-4xl font-bold bg-gradient-to-br to-gray-400 via-gray-500 from-gray-300 bg-clip-text text-transparent z-10">
               Draw what you Desire!
             </h1>
 
@@ -182,51 +237,56 @@ export default function App() {
 
 
             {/* Summary */}
-            <p className="text-center text-white text-md max-w-xl opacity-80">
+            <p className="text-center text-white text-md max-w-xl opacity-80 z-10">
               A minimal interactive drawing interface built using modern frontend tools. Users can draw shapes, lines, arrows, and more using canvas support with animations and responsive UI.
             </p>
 
 
 
             {/* Tech Boxes */}
-            <div className="flex flex-wrap gap-6 justify-center w-[420px]">
+            <div className="flex flex-wrap gap-4 justify-center w-[420px] z-10">
 
               {/* Framework */}
-              <div className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10">
+              <a className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10 hover:border-white/30" href="https://react.dev/" target="blank">
                 <p className="text-sm font-semibold opacity-70">Framework</p>
                 <p className="text-md">React <span className="text-xs opacity-70">version 19.0.0</span></p>
                 <p className="text-md">Vite <span className="text-xs opacity-70">version 6.3.1</span></p>
-              </div>
+              </a>
 
               {/* Styling */}
-              <div className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10">
+              <a className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10 hover:border-white/30" href="https://tailwindcss.com/" target="blank">
                 <p className="text-sm font-semibold opacity-70">UI Design</p>
                 <p className="text-md"><span className="opacity-70">Styling </span>- TailwindCSS</p>
                 <p className="text-md"><span className="opacity-70">Icons -</span> Lucide | React-Icons</p>
-              </div>
+              </a>
 
               {/* Animations */}
-              <div className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10">
+              <a className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10 hover:border-white/30" href="https://motion.dev/docs/react-animation" target="blank">
                 <p className="text-sm font-semibold opacity-70">Animations</p>
                 <p className="text-md">Framer Motion</p>
                 <p className="text-md">Tailwind Animations</p>
-              </div>
+              </a>
 
               {/* Canvas */}
-              <div className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10">
+              <a className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10 hover:border-white/30" href="https://konvajs.org/docs/react/index.html" target="blank">
                 <p className="text-sm font-semibold opacity-70">Canvas</p>
                 <p className="text-md">Konva <span className="text-xs opacity-70">version 9.3.20</span></p>
                 <p className="text-md">React-Konva <span className="text-xs opacity-70">version 19.0.3</span></p>
+              </a>
+
+              {/* Canvas */}
+              <div className="px-4 py-3 text-white rounded-xl shadow-2xl bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 backdrop-blur-lg border border-white/10 hover:border-white/30 ">
+                <p className="text-md">Deployed with the help of GitPages </p>
               </div>
             </div>
 
             {/* Buttons */}
             <div className="flex gap-4 pt-6">
-              <button className="px-5 py-2 bg-gradient-to-br from-gray-400/20 via-gray-600/20 to-blue-300/10 text-white rounded-xl shadow-lg transition hover:scale-105 duration-300">
-                View Other Projects
+              <button className="px-5 py-2 bg-transparent hover:bg-gradient-to-br hover:from-gray-400/20 hover:via-gray-600/20 hover:to-blue-300/10 text-white rounded-xl shadow-lg transition hover:scale-105 duration-300 z-20 border border-gray-500">
+                <a href="https://github.com/SahilSharma1212" target="blank" className="flex items-center gap-2">View Other Projects <FolderKanban strokeWidth={1}/></a>
               </button>
-              <button className="px-5 py-2 bg-white text-gray-800 font-semibold rounded-xl shadow-lg transition hover:scale-105 duration-300">
-                Start Drawing
+              <button className="px-5 py-2 bg-white hover:bg-gray-300 text-black font-semibold rounded-xl shadow-lg transition hover:scale-105 duration-300 z-20 flex gap-2">
+                Start Drawing <ChevronRight />
               </button>
             </div>
           </div>
@@ -238,6 +298,6 @@ export default function App() {
 
 
       </div>
-    </>
+    </div>
   );
 }
