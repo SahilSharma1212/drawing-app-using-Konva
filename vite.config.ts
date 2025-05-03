a7201920c096f2470baf3d4f26dc7e06b4888db8
Harry <sahilbhaisharma1212@gmail.com>
2025-05-04 02:04:18 +0530

    gitpages deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  base: 'drawing-app-using-Konva',
+  base: '/drawing-app-using-Konva/',
 })
